36c9d6fd18048b9384fb2884160ef76095b01e88
Remy Parzinski <remypar5@users.noreply.github.com>
2017-12-20 16:15:25 +0100

    Fix the issue with tests not being able to run (#6)
    * Fix issues with running tests
    
    * Move test files to __test__ folders
    
    * Create ContainerComponents for screens
    
    * Use jest/enzyme as testing framework
    
    * Move navigationOptions to container components
    
    * Shorten test commands
    
    * 100% coverage NumberSpinner
    
    * As much coverage as possible SpinnerButton
    
    * 100% coverage for SpinnerButton component
    
    * Skip coverage minimum for screen containers for now

--- js/components/__test__/NumberSpinner.test.js
+++ js/components/__test__/NumberSpinner.test.js
@@ -10,7 +10,7 @@ describe('<NumberSpinner />', () => {
     beforeEach(() => {
         component = shallow(
             <NumberSpinner
-                value={3} />
+                value={20} />
         );
     });
 
@@ -25,26 +25,78 @@ describe('<NumberSpinner />', () => {
             .props
             .children
         )
-        .toBe(3);
+        .toBe(20);
     });
 
-    it.skip('should be able to increase or decrease the value by pressing the spinner buttons', () => {
-        const render = renderer.create(<NumberSpinner value={3} />);
-        const component = render.toJSON();
+    describe('changing the value', () => {
+        it('should be able to decrease the value by 1 by pressing the down spinner button', () => {
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("20");
 
-        // Simulate clicking a button to change the value
-        expect(component.children[1].children[0]).toBe('4');
+            component.find('SpinnerButton').at(0).simulate('press');
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("19");
+        });
+
+        it('should be able to increase the value by 1 by pressing the up spinner button', () => {
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("20");
+
+            component.find('SpinnerButton').at(1).simulate('press');
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("21");
+        });
+
+        it('should be able to decrease the value by 10 by holding the down spinner button', () => {
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("20");
+
+            component.find('SpinnerButton').at(0).simulate('hold');
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("10");
+        });
+
+        it('should be able to increase the value by 10 by holding the up spinner button', () => {
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("20");
+
+            component.find('SpinnerButton').at(1).simulate('hold');
+            expect(component.find('Text').at(0).childAt(0).text()).toBe("30");
+        });
+
+        it('should call the onChange event listener', () => {
+            const onChangeSpy = jest.fn();
+            const component = shallow(
+                <NumberSpinner
+                    value={20}
+                    onChange={ onChangeSpy } />
+            );
+
+            component.find('SpinnerButton').at(1).simulate('press');
+            expect(onChangeSpy).toHaveBeenCalledWith(21);
+        });
+
+        describe('limitations', () => {
+            beforeEach(() => {
+                component = shallow(
+                    <NumberSpinner
+                        value={20}
+                        min={19}
+                        max={21} />
+                );
+            });
+
+            it('should not exceed the provided min value', () => {
+                component.find('SpinnerButton').at(0).simulate('press');
+                expect(component.find('Text').at(0).childAt(0).text()).toBe("19");
+
+                // Do it again
+                component.find('SpinnerButton').at(0).simulate('press');
+                expect(component.find('Text').at(0).childAt(0).text()).toBe("19");
+            });
+
+            it('should not exceed the provided max value', () => {
+                component.find('SpinnerButton').at(1).simulate('press');
+                expect(component.find('Text').at(0).childAt(0).text()).toBe("21");
+
+                // Do it again
+                component.find('SpinnerButton').at(1).simulate('press');
+                expect(component.find('Text').at(0).childAt(0).text()).toBe("21");
+            });
+        });
     });
 
-    it.skip('should call the onChange event listener', () => {
-        const onChangeSpy = jest.fn();
-        const render = renderer.create(
-            <NumberSpinner
-                value={3}
-                onChange={onChangeSpy} />
-        );
-
-        // Simulate clicking a button to change the value
-        expect(onChangeSpy).toHaveBeenCalled();
-    });
 });
