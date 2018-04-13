c34cf0004ed3742cd9f66c6c2aa599c279eebe5f
Remy Parzinski <rs.parzinski@gmail.com>
2018-04-13 19:50:46 +0200

    fix and unskip failing test on PlayerPropTypes

--- js/types/PlayerPropType.js
+++ js/types/PlayerPropType.js
@@ -1,11 +1,11 @@
-import { boolean, number, shape, string } from 'prop-types';
+import { bool, number, shape, string } from 'prop-types';
 
 const PlayerPropType = shape({
     id: number.isRequired,
     name: string.isRequired,
     life: number.isRequired,
     color: string,
-    isGameOver: boolean,
+    isGameOver: bool,
 });
 
 export default PlayerPropType;
